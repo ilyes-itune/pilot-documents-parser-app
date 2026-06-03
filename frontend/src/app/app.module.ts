4c2122d5bf4dadb125d3ce54fd8fedfa86ba979d
ilyes-itune <ilyes.itune@gmail.com>
2026-06-03 14:20:26 +0200

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,39 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatFileUploadModule } from '@angular/material/file-upload';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatTableModule } from '@angular/material/table';
-import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 import { AppComponent } from './app.component';
-import { UploadComponent } from './components/upload/upload.component';
-import { ParserResultsComponent } from './components/parser-results/parser-results.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UploadComponent,
-    ParserResultsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatButtonModule,
-    MatCardModule,
-    MatFileUploadModule,
-    MatProgressBarModule,
-    MatTableModule,
+    MatToolbarModule,
     MatTabsModule,
-    MatToolbarModule
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatListModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
